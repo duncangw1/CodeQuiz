e0537e21a052a49e18b665f4bea15026f1b039d8
WD40 <69653174+duncangw1@users.noreply.github.com>
2020-09-23 17:57:27 -0400

    Created an array of objects holding the quiz questions, choices, and answers.

---
+++ assets/quiz.js
@@ -0,0 +1,46 @@
+// Object holding questions, choices, and answers
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+  {
+    question:
+      "The condition in an if/else statement is enclosed within ______.",
+    choices: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets",
+    ],
+    answer: "3. parentheses",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ______.",
+    choices: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    answer: "4. all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within ______ when being assigned to variables.",
+    choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log",
+    ],
+    answer: "4. console.log",
+  },
+];
